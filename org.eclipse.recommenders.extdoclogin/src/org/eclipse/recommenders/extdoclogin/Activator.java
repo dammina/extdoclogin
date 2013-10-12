3e85c4b3b66870e6528699b03747943a711c3019
dammina <dmsahabandu@gmail.com>
2013-10-12 23:37:21 +0530

    Update Activator.java

--- org.eclipse.recommenders.extdoclogin/src/org/eclipse/recommenders/extdoclogin/Activator.java
+++ org.eclipse.recommenders.extdoclogin/src/org/eclipse/recommenders/extdoclogin/Activator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.eclipse.recommenders.extdoclogin;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -60,66 +59,3 @@ public class Activator extends AbstractUIPlugin {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 }
-=======
-package org.eclipse.recommenders.extdoclogin;
-
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
-
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractUIPlugin {
-
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.recommenders.extdoclogin"; //$NON-NLS-1$
-
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
-}
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
