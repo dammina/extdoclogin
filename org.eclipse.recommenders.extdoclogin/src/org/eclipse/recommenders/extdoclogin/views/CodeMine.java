8bdc5eacb1d36f2104f395d8eea3664e9d8a6061
dammina <dmsahabandu@gmail.com>
2013-10-12 23:34:13 +0530

    Update CodeMine.java

--- org.eclipse.recommenders.extdoclogin/src/org/eclipse/recommenders/extdoclogin/views/CodeMine.java
+++ org.eclipse.recommenders.extdoclogin/src/org/eclipse/recommenders/extdoclogin/views/CodeMine.java
@@ -6,33 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream.GetField;
 
-<<<<<<< HEAD
 /**
  * Contains some methods which are defined to data mine from the selected file
  *
  * @author Dammina Sahabandu
  */
 
-=======
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 public class CodeMine {
 	public static String dataString;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 /*		try {
-=======
-		try {
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 			new CodeMine().getclasses("/home/dammina/driveapp_v1/org.eclipse.recommenders.extdoclogin/src/org/eclipse/recommenders/extdoclogin/views/ExtdocView.java");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-<<<<<<< HEAD
 		}*/
-=======
-		}
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 
 	}
 	public String getclasses(String path) throws IOException{
@@ -43,7 +32,6 @@ public class CodeMine {
 			//    		System.out.println(path);
 			boolean jdoc=false;
 			String line;
-<<<<<<< HEAD
 
 			/*while ((line = br.readLine()) != null) {
 				CodeMine.dataString+=line+"\n";
@@ -54,18 +42,6 @@ public class CodeMine {
 				if(DocEditor.javadoc){
 					if(line.contains("/**")){
 						CodeMine.dataString+="\n\n\n";
-=======
-			
-			/*while ((line = br.readLine()) != null) {
-				CodeMine.dataString+=line+"\n";
-			}*/
-			
-			
-			while ((line = br.readLine()) != null) {
-				if(testui.javadoc){
-					if(line.contains("/**")){
-						CodeMine.dataString+="\n\n\n\n\n\n";
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 						jdoc=true;
 					}
 					/*else if(jdoc && line.con("*")){
@@ -77,19 +53,11 @@ public class CodeMine {
 					if(jdoc)
 						CodeMine.dataString+=line+"\n";
 				}
-<<<<<<< HEAD
 				if((line.startsWith("class") || line.startsWith("public class")) && DocEditor.classes){
 					//    				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(testui.class.getProtectionDomain().getCodeSource().getLocation().getPath().toString()+"src/Document.txt", true)));
 
 
 					if(!DocEditor.methods){
-=======
-				if((line.startsWith("class") || line.startsWith("public class")) && testui.classes){
-					//    				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(testui.class.getProtectionDomain().getCodeSource().getLocation().getPath().toString()+"src/Document.txt", true)));
-
-
-					if(!testui.methods){
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 						//    					out.println(line.substring(0, line.length()-1)+"\n \n \n \n");
 						CodeMine.dataString+=line.substring(0, line.length()-1)+"\n \n \n\t\t\t\t\t***\t\t\t\t\t\n\n \n";
 					}
@@ -98,11 +66,7 @@ public class CodeMine {
 						CodeMine.dataString+="\n\t\t\t\t\t***\t\t\t\t\t\n"+line.substring(0, line.length()-1)+"\n";
 					}
 					//    				out.close();
-<<<<<<< HEAD
 				}else if(line.contains("public") && line.contains("(") && line.contains(")") && DocEditor.methods){
-=======
-				}else if(line.contains("public") && line.contains("(") && line.contains(")") && testui.methods){
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 					//    				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(testui.class.getProtectionDomain().getCodeSource().getLocation().getPath().toString()+"src/Document.txt", true)));
 					//    				out.println("\t"+line.substring(0, line.length()-1)+"\n");
 					CodeMine.dataString+="\n\t"+line.substring(0, line.length()-1)+"\n";
@@ -110,15 +74,9 @@ public class CodeMine {
 				}
 			}
 			br.close();
-<<<<<<< HEAD
 
 		}
 		//		System.out.println(CodeMine.dataString);
-=======
-			
-		}
-//		System.out.println(CodeMine.dataString);
->>>>>>> 0551bec1a5ec598c2b0cf9f0409ccc78686b8568
 		return dataString;
 	}
 
